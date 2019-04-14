2d3289b73f97c566222cfe667b90ff16510eb19a
Jennifer <jennifer.boyd@mead354.org>
2019-04-14 09:58:24 -0700

    oh yes I did

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Jennifer Boyd on 4/14/19.
+//  Copyright © 2019 Riverpoint Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let message = "Hello Git!"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(message)
+        
+       
+    }
+
+
+}
+
